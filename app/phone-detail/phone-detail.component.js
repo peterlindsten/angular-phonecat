c3d49a24c46ef74340916ac3ac065977fba5c1d1
plindsten <app1337@gmail.com>
2016-10-05 07:12:30 +0200

    extras

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -5,16 +5,31 @@ angular.
   module('phoneDetail').
   component('phoneDetail', {
     templateUrl: 'phone-detail/phone-detail.template.html',
-    controller: ['$routeParams', 'Phone',
-      function PhoneDetailController($routeParams, Phone) {
+    controller: ['$routeParams', 'Phone', '$timeout',
+      function PhoneDetailController($routeParams, Phone, $timeout) {
         var self = this;
-        self.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-          self.setImage(phone.images[0]);
-        });
+        $timeout(function() {
+          self.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+            self.setImage(phone.images[0]);
+          });
+        }, randn_bm() * 1500);
 
         self.setImage = function setImage(imageUrl) {
-          self.mainImageUrl = imageUrl;
+          $timeout(function() {
+            self.mainImageUrl = imageUrl;
+          }, Math.random() * 1000 + 1000);
         };
       }
     ]
   });
+
+  // Standard Normal variate using Box-Muller transform.
+  function randn_bm() {
+      var u = 1 - Math.random(); // Subtraction to flip [0, 1) to (0, 1].
+      var v = 1 - Math.random();
+      var r = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+      if (r < 0) {
+        r = -r;
+      }
+      return r;
+  }
